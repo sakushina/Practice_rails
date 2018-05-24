639847a45558d5dd53ad5b5828bea6f1bbb88630
yuri <sakurashinamonn@yahoo.co.jp>
2018-05-25 03:09:20 +0900

    add: weight temp migration

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,4 +2,6 @@ class Message < ApplicationRecord
   validates :title , length: {maximum: 32},presence:true
   validates :message, length: {maximum: 256},presence:true
   validates :name, length: {maximum: 32}
+  validates :weight, presence: true
+  validates :temp, presence: true
 end
